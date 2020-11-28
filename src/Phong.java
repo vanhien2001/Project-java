082599fad513db958a16f02249b597e65be65855
khang phung <duykhangpdk0701@gmail.com>
2020-11-29 00:35:02 +0700

    them class phong

---
+++ src/Phong.java
@@ -0,0 +1,47 @@
+public class Phong {
+  private String maPhong;
+  private String tenPhong;
+  private String maLoai;
+  private String tinhTrang;
+  private String soKhachHangToiDa;
+
+  public String getMaPhong() {
+    return maPhong;
+  }
+
+  public void setMaPhong(String maPhong) {
+    this.maPhong = maPhong;
+  }
+
+  public String getTenPhong() {
+    return tenPhong;
+  }
+
+  public void setTenPhong(String tenPhong) {
+    this.tenPhong = tenPhong;
+  }
+
+  public String getMaLoai() {
+    return maLoai;
+  }
+
+  public void setMaLoai(String maLoai) {
+    this.maLoai = maLoai;
+  }
+
+  public String getTinhTrang() {
+    return tinhTrang;
+  }
+
+  public void setTinhTrang(String tinhTrang) {
+    this.tinhTrang = tinhTrang;
+  }
+
+  public String getSoKhachHangToiDa() {
+    return soKhachHangToiDa;
+  }
+
+  public void setSoKhachHangToiDa(String soKhachHangToiDa) {
+    this.soKhachHangToiDa = soKhachHangToiDa;
+  }
+}
