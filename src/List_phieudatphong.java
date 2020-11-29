07b3625938875b1ef953ef0819819985988befde
Văn Hiền Nguyễn <vanhienit2001@gmail.com>
2020-11-29 15:59:55 +0700

    add class list_phieudatphong

---
+++ src/List_phieudatphong.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class List_phieudatphong {
+    Phieudatphong[] arrBooked;
+    Scanner sc = new Scanner(System.in);
+
+    public List_phieudatphong() {
+        arrBooked = new Phieudatphong[0];
+    }
+
+    public void Dat_phong(){
+        Phieudatphong a;
+        arrBooked = Arrays.copyOf(arrBooked, arrBooked.length + 1) 
+        a.Dat_phong();
+        arrBooked[arrBooked.length-1]=a;
+    }
+
+    public void Xuatdsphieu() {
+        for (Phieudatphong booked : arrBooked) {
+            booked.Xuat_thong_tin();
+        }
+    }
+
+
+}
